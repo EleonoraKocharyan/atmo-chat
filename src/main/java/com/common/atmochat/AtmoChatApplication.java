c74e6f37331f1e3fded6a0d47657670b43d5d32d
eleonorakocharyan <eleonora.kocharyan@gmail.com>
2018-11-19 14:10:34 +0400

    Chat connection established, user creation needs to be fixed

--- src/main/java/com/common/atmochat/AtmoChatApplication.java
+++ src/main/java/com/common/atmochat/AtmoChatApplication.java
@@ -1,20 +1,93 @@
+//package com.common.atmochat;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.socket.config.annotation.EnableWebSocket;
+//
+//@EnableAutoConfiguration(exclude = {
+//        SecurityAutoConfiguration.class})
+//@Configuration
+//@ComponentScan
+////@EnableWebSocket
+//public class AtmoChatApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(AtmoChatApplication.class, args);
+//	}
+//}
+
+
 package com.common.atmochat;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
+		import org.atmosphere.cpr.AtmosphereServlet;
+		import org.atmosphere.cpr.ContainerInitializer;
+		import org.springframework.boot.SpringApplication;
+		import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+		import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+		import org.springframework.boot.web.servlet.ServletContextInitializer;
+		import org.springframework.boot.web.servlet.ServletRegistrationBean;
+		import org.springframework.context.annotation.Bean;
+		import org.springframework.context.annotation.ComponentScan;
+		import org.springframework.context.annotation.Configuration;
+		import org.springframework.core.Ordered;
+		import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+		import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+		import javax.servlet.ServletContext;
+		import javax.servlet.ServletException;
+		import java.util.Collections;
 
 @EnableAutoConfiguration(exclude = {
-        SecurityAutoConfiguration.class})
+		SecurityAutoConfiguration.class})
 @Configuration
 @ComponentScan
 //@EnableWebSocket
 public class AtmoChatApplication {
 
+
+	@Bean
+	public EmbeddedAtmosphereInitializer atmosphereInitializer() {
+		return new EmbeddedAtmosphereInitializer();
+	}
+
+	@Bean
+	public ServletRegistrationBean atmosphereServlet() {
+// Dispatcher servlet is mapped to '/home' to allow the AtmosphereServlet
+// to be mapped to '/chat'
+		ServletRegistrationBean registration = new ServletRegistrationBean(
+				new AtmosphereServlet(), "/chat/*");
+//		registration.addInitParameter("org.atmosphere.cpr.packages", "sample");
+		registration.addInitParameter("org.atmosphere.interceptor.HeartbeatInterceptor"
+				+ ".clientHeartbeatFrequencyInSeconds", "10");
+		registration.setLoadOnStartup(0);
+// Need to occur before the EmbeddedAtmosphereInitializer
+		registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return registration;
+	}
+
+//	@Configuration
+//	static class MvcConfiguration extends WebMvcConfigurerAdapter {
+//
+//	@Override
+//	public void addViewControllers(ViewControllerRegistry registry) {
+//	registry.addViewController("/").setViewName("forward:/home/home.html");
+//	}
+//
+//	}
+
+	private static class EmbeddedAtmosphereInitializer extends ContainerInitializer
+			implements ServletContextInitializer {
+
+		@Override
+		public void onStartup(ServletContext servletContext) throws ServletException {
+			onStartup(Collections.<Class<?>> emptySet(), servletContext);
+		}
+
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(AtmoChatApplication.class, args);
 	}

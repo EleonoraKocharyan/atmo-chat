4142ac56812751338fc5c267bb7d7c1b6ae51e80
eleonorakocharyan <eleonora.kocharyan@gmail.com>
2018-12-04 12:24:46 +0400

    Tests commented

--- src/test/java/com/common/atmochat/AtmoChatApplicationTests.java
+++ src/test/java/com/common/atmochat/AtmoChatApplicationTests.java
@@ -1,16 +1,16 @@
-package com.common.atmochat;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class AtmoChatApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
+//package com.common.atmochat;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class AtmoChatApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
